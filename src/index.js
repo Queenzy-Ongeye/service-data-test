990114c073efe29bd525cfb4b7fd3ef2fe6da64b
Queenzy-Ongeye <queenzyblack176@gmail.com>
2025-02-13 10:58:58 +0300

    installed vconsole

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import VConsole from 'vconsole';
+
+new VConsole("Production");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
